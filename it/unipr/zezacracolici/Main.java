df8617eb89e17c84c8d6abec2f5a8ee1f8ede19a
cracolicil <leonardo.cracolici@gmail.com>
2021-10-17 20:34:11 +0200

    Aggiunto inserimento dati in un file

--- it/unipr/zezacracolici/Main.java
+++ it/unipr/zezacracolici/Main.java
@@ -3,6 +3,15 @@
  */
 package it.unipr.zezacracolici;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.Scanner;
 
 /**
@@ -12,17 +21,19 @@ import java.util.Scanner;
  */
 public class Main {
 
+	public static final String DATAFILE = "users.txt";
+	
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner reader = new Scanner(System.in);
 		int n=0;
 		do
 		{
 			System.out.println("0:Quit\n1:Register\n2:LogIn");
 			n = reader.nextInt();
-			//System.out.println();
 			switch(n) {
 				case 0:
 					break;
@@ -31,6 +42,15 @@ public class Main {
 					String username = reader.next();
 					System.out.print("Insert password: ");
 					String password = reader.next();
+					String user = username+","+password+",user";
+					DataOutputStream fObj = null;
+					try {
+				        fObj = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(DATAFILE, true)));
+				        fObj.writeUTF(user);
+					}
+					finally {
+						fObj.close();
+					}
 					break;
 				case 2:
 					//operazioni di login
@@ -40,5 +60,4 @@ public class Main {
 		while(n!=0);
 		reader.close();
 	}
-
 }
