31b0451af50b834e35eb172a9c21af5d99dcf239
Leonardo Cracolici <leonardo.cracolici@gmail.com>
2021-10-28 08:46:16 +0200

    creato il javadoc del packageinfo

--- it/unipr/zezacracolici/package-info.java
+++ it/unipr/zezacracolici/package-info.java
@@ -1,9 +1,12 @@
 /**
+ * Provides the classes necessary to create an app and the classes an app uses 
+ * to communicate with its app context.
  * 
- */
-/**
  * @author enize
  * @author leocraco
+ * 
+ * @version  1.0
+ * @since    1.0
  *
  */
 package it.unipr.zezacracolici;
